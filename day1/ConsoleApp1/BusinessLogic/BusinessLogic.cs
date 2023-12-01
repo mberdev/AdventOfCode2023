22bd2b951f3c3a2404dcc025bd372bfc49088e4f
monsieur xx <jeancallisti@yahoo.fr>
2023-12-02 00:36:00 +0100

    cleanup

--- day1/ConsoleApp1/BusinessLogic/BusinessLogic.cs
+++ day1/ConsoleApp1/BusinessLogic/BusinessLogic.cs
@@ -11,13 +11,22 @@ namespace ConsoleApp1.BusinessLogic
 {
     public static class BusinessLogic
     {
-        private static string[] FindDigits(string s, RegexOptions option)
+        private static string pattern = $"(?:[0-9]|{string.Join("|", Digits.DigitWords)})";
+        private static Regex r_LeftToRight = new Regex(pattern, RegexOptions.None);
+        private static Regex r_RightToLeft = new Regex(pattern, RegexOptions.RightToLeft);
+
+        private static string[] FindDigits_LeftToRight(string s)
         {
-            string pattern = @"(?:[0-9]|zero|one|two|three|four|five|six|seven|eight|nine)";
-            var r = new Regex(pattern, option);
-            MatchCollection matches = r.Matches(s);
+            MatchCollection matches = r_LeftToRight.Matches(s);
             return matches.ToArray().Select(m => m.Value).ToArray();
         }
+
+        private static string[] FindDigits_RightToLeft(string s)
+        {
+            MatchCollection matches = r_RightToLeft.Matches(s);
+            return matches.ToArray().Select(m => m.Value).ToArray();
+        }
+
 
         private static int SingleDigitToInt(string s)
         {
@@ -34,8 +43,8 @@ namespace ConsoleApp1.BusinessLogic
 
         public static int FindRowDigits_v1(string s)
         {
-            var first = FindDigits(s, RegexOptions.None).First();
-            var last = FindDigits(s, RegexOptions.RightToLeft).First();
+            var first = FindDigits_LeftToRight(s).First();
+            var last = FindDigits_RightToLeft(s).First();
             var firstAsInt  = DigitToInt(first);
             var lastAsInt = DigitToInt(last);
             return int.Parse($"{firstAsInt}{lastAsInt}");

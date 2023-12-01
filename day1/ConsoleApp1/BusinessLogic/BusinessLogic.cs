a6288c8425e107793a617dedd04548e8251ad56c
monsieur xx <jeancallisti@yahoo.fr>
2023-12-02 00:29:52 +0100

    solution for day1 - part 2

---
+++ day1/ConsoleApp1/BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleApp1.InputParse;
+using ConsoleApp1.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.BusinessLogic
+{
+    public static class BusinessLogic
+    {
+        private static string[] FindDigits(string s, RegexOptions option)
+        {
+            string pattern = @"(?:[0-9]|zero|one|two|three|four|five|six|seven|eight|nine)";
+            var r = new Regex(pattern, option);
+            MatchCollection matches = r.Matches(s);
+            return matches.ToArray().Select(m => m.Value).ToArray();
+        }
+
+        private static int SingleDigitToInt(string s)
+        {
+            return int.Parse($"{s}");
+        }
+
+
+
+        private static int DigitToInt(string s)
+        {
+            return s.Length == 1 ? SingleDigitToInt(s) : Digits.WordDigitToInt(s);
+        }
+
+        private static int[] DigitsAsInts(string[] digits)
+        {
+            return digits.Select(d => DigitToInt(d)).ToArray();
+        }
+
+        public static int FindRowDigits_v1(string s)
+        {
+            var first = FindDigits(s, RegexOptions.None).First();
+            var last = FindDigits(s, RegexOptions.RightToLeft).First();
+            string[] both = { first, last };
+            var asInt  = DigitsAsInts(both);
+            return int.Parse($"{asInt.First()}{asInt.Last()}");
+        }
+
+    }
+}

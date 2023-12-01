0d72488b0017a435677bcdbaf614af2c19e51556
monsieur xx <jeancallisti@yahoo.fr>
2023-12-01 23:31:47 +0100

    basic boilerplate

--- day1/ConsoleApp1/Program.cs
+++ day1/ConsoleApp1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using ConsoleApp1.InputParse;
+
+var lines = LineBreakParser.ParseInput(Input.File);
+lines.ToList().ForEach(l => Console.WriteLine(l));

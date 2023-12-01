246a9bfa48da271ec9af82a718a40c377086f496
monsieur xx <jeancallisti@yahoo.fr>
2023-12-02 00:38:02 +0100

    day2 - boilerplate from day1

---
+++ day2/ConsoleApp1/BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,54 @@
+﻿using ConsoleApp1.InputParse;
+using ConsoleApp1.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.BusinessLogic
+{
+    public static class BusinessLogic
+    {
+        private static string pattern = $"(?:[0-9]|{string.Join("|", Digits.DigitWords)})";
+        private static Regex r_LeftToRight = new Regex(pattern, RegexOptions.None);
+        private static Regex r_RightToLeft = new Regex(pattern, RegexOptions.RightToLeft);
+
+        private static string[] FindDigits_LeftToRight(string s)
+        {
+            MatchCollection matches = r_LeftToRight.Matches(s);
+            return matches.ToArray().Select(m => m.Value).ToArray();
+        }
+
+        private static string[] FindDigits_RightToLeft(string s)
+        {
+            MatchCollection matches = r_RightToLeft.Matches(s);
+            return matches.ToArray().Select(m => m.Value).ToArray();
+        }
+
+
+        private static int SingleDigitToInt(string s)
+        {
+            return int.Parse($"{s}");
+        }
+
+
+
+        private static int DigitToInt(string s)
+        {
+            return s.Length == 1 ? SingleDigitToInt(s) : Digits.WordDigitToInt(s);
+        }
+
+
+        public static int FindRowDigits_v1(string s)
+        {
+            var first = FindDigits_LeftToRight(s).First();
+            var last = FindDigits_RightToLeft(s).First();
+            var firstAsInt  = DigitToInt(first);
+            var lastAsInt = DigitToInt(last);
+            return int.Parse($"{firstAsInt}{lastAsInt}");
+        }
+
+    }
+}
---
+++ day2/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using ConsoleApp1.BusinessLogic;
+using ConsoleApp1.InputParse;
+using System.Linq;
+
+var lines = LineBreakParser.ParseInput(Input.File);
+foreach (var line in lines)
+{
+    //Console.WriteLine($"{line} -> {BusinessLogic.FindRowDigits_v1(line)}");
+}
+
+var sum = lines.ToList().Select(l => BusinessLogic.FindRowDigits_v1(l)).Sum();
+Console.WriteLine(sum);

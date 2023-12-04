d964512fa1c09cd03e003218e7a85bd92fb4af99
monsieur xx <jeancallisti@yahoo.fr>
2023-12-04 11:15:29 +0100

    day 4 boilerplate (copy-paste from day 3)

---
+++ day4/ConsoleApp1/InputParse/LineDataParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.InputParse
+{
+    public static class LineDataParser
+    {
+        public static List<Token> ParseTokens(int row, string input)
+        {
+            // Note: The two regex below could have been combined.
+
+            // Parsing integers
+            var numbers = Regex.Matches(input, @"\d+");
+            var tokens = numbers.Select(m => new Token(row, m.Index, m.Value)).ToList();
+
+            // Parsing one-character symbols
+            var symbols = Regex.Matches(input, @"[^.\s\d]");
+            tokens.AddRange(
+                symbols.Select(m => new Token(row, m.Index, m.Value))
+            );
+
+            return tokens;
+        }
+        
+    }
+}
---
+++ day4/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using ConsoleApp1.BusinessLogic;
+using ConsoleApp1.InputParse;
+using System.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+var lines = LineBreakParser.ParseInput(Input.File);
+
+static void Part2(string[] lines)
+{
+
+    var map = new TokensMap();
+    int row = 0;
+    foreach (var line in lines)
+    {
+        map.Tokens.AddRange(LineDataParser.ParseTokens(row, line));
+        row++;
+    }
+
+    var sum = 0;
+
+    foreach (var symbol in map.Tokens.Where(t => t.IsSymbol))
+    {
+        var adjacentNumbers = BusinessLogic.NumbersAdjacentToSymbol(map, symbol);
+        if (adjacentNumbers.Count == 2)
+        {
+            sum += adjacentNumbers[0].AsNumber * adjacentNumbers[1].AsNumber;
+        }
+    }
+
+    Console.WriteLine($"=============================");
+
+    Console.WriteLine($"");
+    Console.WriteLine($"");
+    Console.WriteLine(sum);
+    Console.WriteLine($"");
+    Console.WriteLine($"");
+}
+
+//Part1(lines);
+Part2(lines);

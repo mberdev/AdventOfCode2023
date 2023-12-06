fb903fd883f4ae7705500a7bdf82f9d2c7230fca
monsieur xx <jeancallisti@yahoo.fr>
2023-12-06 13:41:32 +0100

    part 2 data parsing

--- day6/ConsoleApp1/InputParse/LineDataParser.cs
+++ day6/ConsoleApp1/InputParse/LineDataParser.cs
@@ -10,12 +10,25 @@ namespace ConsoleApp1.InputParse
     public class DataParser
     {
 
-        public static List<Record> Parse(string times, string distances)
+        public static List<Record> Parse_Part1(string times, string distances)
         {
             var strTimes = times.Split(' ').Skip(1).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
             var strDistances = distances.Split(' ').Skip(1).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
 
             return strTimes.Zip(strDistances, (time, distance) => new Record(int.Parse(time), int.Parse(distance))).ToList();
+        }
+
+        public static Record Parse_Part2(string times, string distances)
+        {
+            var strTimes = times.Split(' ').Skip(1).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+            var strTime = string.Join("", strTimes);
+            var time = int.Parse(strTime);
+
+            var strDistances = distances.Split(' ').Skip(1).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+            var strDistance = string.Join("", strDistances);
+            var distance = int.Parse(strDistance);
+
+            return new Record(time, distance);
         }
     }
 }
--- day6/ConsoleApp1/Program.cs
+++ day6/ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ static void Part1(string[] lines)
     var times = lines[0];
     var distances = lines[1];
 
-    var records = DataParser.Parse(times, distances);
+    var records = DataParser.Parse_Part1(times, distances);
     Printer.Print(records);
 
     var total = 1;
@@ -44,10 +44,47 @@ static void Part1(string[] lines)
 }
 
 
+static void Part2(string[] lines)
+{
+    var times = lines[0];
+    var distances = lines[1];
+
+    var record = DataParser.Parse_Part2(times, distances);
+    Console.WriteLine(record);
+
+    //var total = 1;
+    //foreach (var record in records)
+    //{
+    //    var race = new Race(record.Time);
+
+    //    int successCount = 0;
+    //    for (int holdTime = 1; holdTime < record.Time; holdTime++)
+    //    {
+
+    //        var myDistance = BusinessLogic.DoRace(race, holdTime);
+    //        if (BusinessLogic.IsRecordBeaten(myDistance, record.Distance))
+    //        {
+    //            successCount++;
+    //        }
+    //    }
+
+    //    total *= successCount;
+
+    //}
+
+    //Console.WriteLine($"=============================");
+
+    //Console.WriteLine($"");
+    //Console.WriteLine($"");
+    //Console.WriteLine(total);
+    //Console.WriteLine($"");
+    //Console.WriteLine($"");
+}
+
 //-------------------------------------------------------------
 
-var lines = LineBreakParser.ParseInput(Input.File, leaveEmptyLines: false);
-//var lines = LineBreakParser.ParseInput(Input.TestSet, leaveEmptyLines: false);
+//var lines = LineBreakParser.ParseInput(Input.File, leaveEmptyLines: false);
+var lines = LineBreakParser.ParseInput(Input.TestSet, leaveEmptyLines: false);
 
-Part1(lines);
-//Part2(lines);
+//Part1(lines);
+Part2(lines);
